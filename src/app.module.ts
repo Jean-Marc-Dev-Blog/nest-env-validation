c395225242d5abc535caf248a973734bb728d040
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-01-27 11:27:56 +0100

    Add schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+        MYSQL_USERNAME: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_SYNCHRONIZE: Joi.boolean().required(),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
